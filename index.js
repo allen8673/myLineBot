41550bc91a0df63da360c55ed4265fdc3e9ab084
WHQ\10704306 <Allen_CL_Lin@wistron.com>
2019-10-17 09:40:58 +0800

    update

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const bot = linebot({
 })
 
 bot.on('message', function (event) {
-    if (event.message.type = 'text') {
+    // if (event.message.type = 'text') 
+    {
         var msg = event.message.text;
         //重覆使用者說的訊息
         event.reply("您說："+msg).then(function (data) {
